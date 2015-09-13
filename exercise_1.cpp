eaaa775654bbfc96ba02ceefd2916b6e264cac1c
irenehgao <irenehgao925052182@gmail.com>
2015-09-13 17:28:45 -0600

    .

--- exercise_1.cpp
+++ exercise_1.cpp
@@ -7,21 +7,19 @@
 int p1ButtonPin = 7;
 int p2ButtonPin = 6;
 int LEDPins[5] = {9,10,11,12,13};
-int LED[5] = { LOW,LOW,LOW,LOW,LOW};
+int LED[5] = {LOW, LOW, LOW, LOW, LOW};
 bool pressed = false;
 
 void setup() {
+  for ( int i = 0;i < 5; i++ ) {
+    pinMode(LEDPins[i],OUTPUT);
+    digitalWrite(LEDPins[i],LED[i]);
+  }
 
-for ( int i = 0;i < 5; i++ ) {
- pinMode(LEDPins[i],OUTPUT);
- digitalWrite(LEDPins[i],LED[i]);
- }
-
-
- pinMode(p1ButtonPin,INPUT);
- digitalWrite(p1ButtonPin,HIGH);
- pinMode(p2ButtonPin,INPUT);
- digitalWrite(p2ButtonPin,HIGH);
+  pinMode(p1ButtonPin,INPUT);
+  digitalWrite(p1ButtonPin,HIGH);
+  pinMode(p2ButtonPin,INPUT);
+  digitalWrite(p2ButtonPin,HIGH);
 }
 
 void loop() {
@@ -29,32 +27,32 @@ void loop() {
   if ((pressed = true) && digitalRead(p1ButtonPin) == LOW) {
       for( int i = 0; i < 5; i++ ) {
         if (LED[i] == LOW) {
-          digitalWrite(LEDPins[i],HIGH);
-          LED[i] == HIGH;
+          digitalWrite(LEDPins[i], HIGH);
+          LED[i] = HIGH;
           break;
         }else if (LED[i] == HIGH) {
-          digitalWrite(LEDPins[i] == LOW);
+          digitalWrite(LEDPins[i], LOW);
           LED[i] = LOW;
         }
       }
   }else if ((pressed = true) && digitalRead(p2ButtonPin) == LOW) {
     for(int i = 0; i < 5; i++) {
       if (LED[i] == LOW) {
-        digitalWrite(LEDPins[i],HIGH);
-        LED[i] == HIGH;
-        break;
+        digitalWrite(LEDPins[i], HIGH);
+        LED[i] = HIGH;
       }else if (LED[i] == HIGH){
-        digitalWrite(LEDPins[i] == LOW);
+        digitalWrite(LEDPins[i], LOW);
         LED[i] = LOW;
+        break;
       }
     }
   }
 
   if ((digitalRead(p1ButtonPin) == LOW)
-    & digitalRead(p2ButtonPin) == LOW) {
+    && digitalRead(p2ButtonPin) == LOW) {
         pressed = true;
     }else if  ((digitalRead(p1ButtonPin) == HIGH)
-      & digitalRead(p2ButtonPin) == HIGH) {
+      && digitalRead(p2ButtonPin) == HIGH) {
         pressed = false;
       }
 }
